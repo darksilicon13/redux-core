8ee2c4d97383cee1d232f2a7f48952f86bc2d75b
darksilicon13 <darksilicon@outlook.com>
2022-08-27 23:51:15 +0900

    Users Page added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import PostsList from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import SinglePostPage from './features/posts/SinglePostPage'
 import EditPostForm from './features/posts/EditPostForm'
+import UserPage from './features/users/UserPage'
+import UsersList from './features/users/UsersList'
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
           } />
           <Route path="/posts/:postId" element={<SinglePostPage />} />
           <Route path="/editPost/:postId" element={<EditPostForm />} />
+          <Route path="/users" element={<UsersList/>}/>
+          <Route path="/users/:userId" element={<UserPage/>}/>
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
